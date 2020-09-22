9a3d26cd86e22cf656825b99a5ddd491ef3bd8ff
MDrapala <drapalamathieu95@gmail.com>
2020-09-22 10:58:21 +0200

    feat(contact): Create contact list with bot

--- js/index.js
+++ js/index.js
@@ -3,6 +3,29 @@ document.getElementById("form").addEventListener("click", function(event){
   event.preventDefault()
 });
 
+var bot = [{
+  id: 0,
+  name: 'BOT Roll',
+  action: '/roll',
+  counter: 0
+}];
+
+document.onload = displayBot();
+
+function displayBot() {
+  var contact = document.querySelector(".contact");
+  for (let i = 0; i < bot.length; i++) {
+    contact.innerHTML += `
+    <div class="contact__profile contact__profile--active">
+      <div class="contact__picture">
+        <img src="./assets/img/bot`+ bot[i].id +`.png" alt="bot" class="contact__picture__img">
+      </div>
+      <span class="contact__name">`+ bot[i].name +`</span>
+      <span class="contact__counter">`+ bot[i].counter +`</span>
+    </div>`
+  }
+}
+
 function sendMessage(){
   var inputVal = document.getElementById("message").value; // Retrieve the user input
   var tchat = document.getElementById("tchat");
